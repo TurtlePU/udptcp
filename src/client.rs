5e40270c5b8506faaa1502f60df1160d9ed4659c
TurtlePU <sokolov.p64@gmail.com>
2021-05-27 19:26:04 +0300

    high-level algorithms

--- src/client.rs
+++ src/client.rs
@@ -1,7 +1,42 @@
-use std::net::ToSocketAddrs;
+use std::{
+    io::{self, Read},
+    net::{ToSocketAddrs, UdpSocket},
+};
 
 use anyhow::Result;
+use itertools::Itertools;
+use rand::Rng;
 
-pub fn start_client(_address: impl ToSocketAddrs) -> Result<()> {
-    todo!()
+pub fn start_client(address: impl ToSocketAddrs) -> Result<()> {
+    let socket = UdpSocket::bind("0.0.0.0:0")?;
+    socket.connect(address)?;
+    let client = Client(socket);
+    let seq = rand::thread_rng().gen_range(0..1000);
+    let mut num = client.start_connection(seq)?;
+    for chunk in &io::stdin().bytes().chunks(1024) {
+        let chunk = chunk.collect::<Result<Vec<_>, _>>()?;
+        num = client.send_chunk(num, &chunk[..])?;
+    }
+    client.end_connection()
+}
+
+struct Numbers {
+    _sequence: u32,
+    _acknowledgment: u32,
+}
+
+struct Client(UdpSocket);
+
+impl Client {
+    fn start_connection(&self, _seq: u32) -> Result<Numbers> {
+        todo!()
+    }
+
+    fn send_chunk(&self, _num: Numbers, _chunk: &[u8]) -> Result<Numbers> {
+        todo!()
+    }
+
+    fn end_connection(&self) -> Result<()> {
+        todo!()
+    }
 }
--- src/server.rs
+++ src/server.rs
@@ -1,7 +1,81 @@
-use std::net::ToSocketAddrs;
+use std::{collections::HashMap, net::SocketAddr, sync::Arc};
 
 use anyhow::Result;
+use rand::Rng;
+use tokio::{
+    net::{ToSocketAddrs, UdpSocket},
+    sync::{Mutex, RwLock},
+};
 
-pub fn start_server(_address: impl ToSocketAddrs) -> Result<()> {
-    Ok(())
+#[tokio::main]
+pub async fn start_server(address: impl ToSocketAddrs) -> Result<()> {
+    let socket = Arc::new(UdpSocket::bind(address).await?);
+    let conns = Arc::new(RwLock::new(Connections::default()));
+    loop {
+        let (packet, addr) = next_packet(&socket).await?;
+        let socket = Arc::clone(&socket);
+        let conns = Arc::clone(&conns);
+        tokio::spawn(async move {
+            let conn = {
+                let conns = conns.read().await;
+                conns.find_addressant(addr)
+            };
+            let conn = match conn {
+                Some(conn) => conn,
+                None => {
+                    let mut conns = conns.write().await;
+                    let ack = rand::thread_rng().gen_range(0..1000);
+                    conns.create_connection(addr, ack)
+                }
+            };
+            let mut conn = conn.lock().await;
+            let verdict = conn.respond_to(&socket, packet).await.unwrap();
+            if let Verdict::_Close = verdict {
+                conns.write().await.remove_connection(addr);
+            }
+        });
+    }
+}
+
+struct Packet;
+
+async fn next_packet(_socket: &UdpSocket) -> Result<(Packet, SocketAddr)> {
+    todo!()
+}
+
+struct Connection {
+    _ack: u32,
+    // ...
+}
+
+enum Verdict {
+    _Keep,
+    _Close,
+}
+
+impl Connection {
+    async fn respond_to(&mut self, _socket: &UdpSocket, _packet: Packet) -> Result<Verdict> {
+        todo!()
+    }
+}
+
+type SyncConnection = Arc<Mutex<Connection>>;
+
+#[derive(Default)]
+struct Connections(HashMap<SocketAddr, SyncConnection>);
+
+impl Connections {
+    fn find_addressant(&self, address: SocketAddr) -> Option<SyncConnection> {
+        self.0.get(&address).cloned()
+    }
+
+    fn create_connection(&mut self, address: SocketAddr, ack: u32) -> SyncConnection {
+        let conn = Arc::new(Mutex::new(Connection { _ack: ack }));
+        self.0.insert(address, Arc::clone(&conn));
+        conn
+    }
+
+    fn remove_connection(&mut self, address: SocketAddr) {
+        self.0.remove(&address);
+    }
 }
